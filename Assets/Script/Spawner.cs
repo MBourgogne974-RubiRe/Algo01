d70ffb6abb5d2c00f1a75aa5b9bed80364015774
samuel moustaquima <s.moustaquima@rubika-edu.re>
2026-09-22 12:55:08 +0400

    samuel v1

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -4,7 +4,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject mob;
 
-    public KeyCode sp = KeyCode.E;
+    public KeyCode sp = KeyCode.M;
 
     public int instant;
 
@@ -18,8 +18,16 @@ public class Spawner : MonoBehaviour
     {
         if (Input.GetKey(sp))
         {
-            Instantiate(mob, new Vector3(5, 1.5f, instant), Quaternion.identity);
-            instant += 1;
+            //Instantiate(mob, new Vector3(5, 1.5f, instant), Quaternion.identity);
+            // instant += 1;
+           /*Instantiate(mob, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(mob, new Vector3(1, 0, 0), Quaternion.identity);
+            Instantiate(mob, new Vector3(2, 0, 0), Quaternion.identity);*/
+
+            for(int i = 0; i < 100; i++)
+            {
+                Instantiate(mob, new Vector3(15, 15, 15)* i, Quaternion.identity);
+            }
         }
     }
 }
